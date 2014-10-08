15735d322ceb4aa3af95c19cf9f587bdf89ac34c
Leo <Leooonard@163.com>
2014-10-08 19:30:03 +0800

    更改了设置里的一些目录， 变成自适应路径

--- mysite/settings.py
+++ mysite/settings.py
@@ -64,13 +64,13 @@ MEDIA_URL = ''
 # Example: "/var/www/example.com/static/"
 
 
-STATIC_DIR= os.path.join(os.getcwd(), 'media').replace('\\', '/')
+STATIC_DIR= os.path.join(os.path.split(os.path.realpath(__file__))[0], 'media').replace('\\', '/')
 
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.getcwd(), 'mysite/media').replace('\\', '/')
+    os.path.join(os.path.split(os.path.realpath(__file__))[0], 'media').replace('\\', '/')
 )
 
 
@@ -103,8 +103,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/sse-server-02/mysite/templates',
-    os.path.join(os.getcwd(), 'templates').replace('\\', '/'),
+    os.path.join(os.path.split(os.path.realpath(__file__))[0], '../templates').replace('\\', '/'),
 )
 
 INSTALLED_APPS = (
