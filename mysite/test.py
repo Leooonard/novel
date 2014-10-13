191569f877b320e32f07531ab912b0cd4cbf24ab
Leo <Leooonard@163.com>
2014-10-13 15:07:51 +0800

    a

--- mysite/test.py
+++ mysite/test.py
@@ -1,9 +1,5 @@
 from ctypes import *
 import ctypes
-bin= cdll.LoadLibrary('/home/mymmoondt/mysite/mysite/lab-ping.so')
-rtval= bin.ping('/home/mymmoondt/mysite/mysite/data')
-bin= cdll.LoadLibrary('/home/mymmoondt/mysite/mysite/read.so')
-rtval= bin.checkPCAP(ctypes.string_at(rtval))
-fd= open('/home/mymmoondt/repos/ns-3-allinone/ns-3-dev/scratch/Event', 'w')
-fd.write(ctypes.string_at(rtval))
-fd.close()
+bin= cdll.LoadLibrary('/home/sse-server-04/git/novel/mysite/lab-ping.so')
+rtval= bin.ping('/home/sse-server-04/git/novel/mysite/ExpResult/092912/data')
+
